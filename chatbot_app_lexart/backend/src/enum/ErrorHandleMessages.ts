708c0a38c9ba96c1b0e33dd9743e8be0f84aa6bf
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-08-26 17:46:06 -0300

    feat: created enum folder

---
+++ chatbot_app_lexart/backend/src/enum/ErrorHandleMessages.ts
@@ -0,0 +1,9 @@
+enum ErrorHandleMessages {
+  NOT_FOUND = "Not found",
+  INVALID_EMAIL_OR_PASSWORD = "Email or password is incorrect.",
+  USER_ALREADY_EXISTS = "User already exists",
+  INTERNAL_ERROR = "An internal error has occurred.",
+  INVALID_ID = "Invalid id",
+}
+
+export default ErrorHandleMessages;
---
+++ chatbot_app_lexart/backend/src/enum/HttpHandleStatus.ts
@@ -0,0 +1,11 @@
+enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  CONFLIT = 409,
+  INTERNAL_ERROR = 500,
+}
+
+export default HttpStatus;
